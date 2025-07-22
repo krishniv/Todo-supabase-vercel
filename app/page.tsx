87c542ad5c26eab385eabf6e4a9bd796add944a2
Krishna Niveditha <krishnaniveditha@Krishnas-MacBook-Pro.local>
2025-07-22 20:12:44 +0530

    build fixes

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
   );
 }
 
-function CheckCircleIcon(props: any) {
+function CheckCircleIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
